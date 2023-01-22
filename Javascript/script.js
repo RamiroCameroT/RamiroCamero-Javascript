23d089daf83b099d49e51fbe0f6be9e2ffcc1a54
Ramiro Camero <ramirocamero12@gmail.com>
2023-01-22 00:34:15 -0300

    .

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,16 +1,16 @@
 const productos = [
-    {id: 1, Nombre: "Fideos con Salsa a Eleccion", Precio: 1800, Categoria: 2,},
-    {id: 2, Nombre: "Milanesa a la Napolitana", Precio: 2100, Categoria: 2},
-    {id: 3, Nombre: "Filet de Merluza con Guarnicion", Precio: 1950, Categoria: 2},
-    {id: 4, Nombre: "Tortilla de Papa", Precio: 1500, Categoria: 1},
-    {id: 5, Nombre: "Entraña con Guarnicion", Precio: 2350, Categoria: 2},
-    {id: 6, Nombre: "Sorrentinos con Salsa a Eleccion", Precio: 1900, Categoria: 2},
-    {id: 7, Nombre: "Volcan de Chocolate", Precio: 900, Categoria: 3},
-    {id: 8, Nombre: "Lemon Pie", Precio: 750, Categoria: 3},
-    {id: 9, Nombre: "Bastones de Muzzarella", Precio: 1000, Categoria: 1},
-    {id: 10, Nombre: "Rabas", Precio: 1300, Categoria: 1},
-    {id: 11, Nombre: "Gaseosas", Precio: 350, Categoria: 4},
-    {id: 12, Nombre: "Vinos", Precio: 500, Categoria: 4},
+    {id: 1, Nombre: "Fideos con Salsa a Eleccion", Precio: 1800, Categoria: 2, img:"./Imagenes/Pollo.jpg"},
+    {id: 2, Nombre: "Milanesa a la Napolitana", Precio: 2100, Categoria: 2, img:"./Imagenes/Pollo.jpg"},
+    {id: 3, Nombre: "Filet de Merluza con Guarnicion", Precio: 1950, Categoria: 2, img:"./Imagenes/Pollo.jpg"},
+    {id: 4, Nombre: "Tortilla de Papa", Precio: 1500, Categoria: 1, img:"./Imagenes/Pollo.jpg"},
+    {id: 5, Nombre: "Entraña con Guarnicion", Precio: 2350, Categoria: 2, img:"./Imagenes/Pollo.jpg"},
+    {id: 6, Nombre: "Sorrentinos con Salsa a Eleccion", Precio: 1900, Categoria: 2, img:"./Imagenes/Pollo.jpg"},
+    {id: 7, Nombre: "Volcan de Chocolate", Precio: 900, Categoria: 3, img:"./Imagenes/Pollo.jpg"},
+    {id: 8, Nombre: "Lemon Pie", Precio: 750, Categoria: 3, img:"./Imagenes/Pollo.jpg"},
+    {id: 9, Nombre: "Bastones de Muzzarella", Precio: 1000, Categoria: 1, img:"./Imagenes/Pollo.jpg"},
+    {id: 10, Nombre: "Rabas", Precio: 1300, Categoria: 1,  img:"./Imagenes/Rabas.jpg"},
+    {id: 11, Nombre: "Gaseosas", Precio: 350, Categoria: 4, img:"./Imagenes/Pollo.jpg"},
+    {id: 12, Nombre: "Vinos", Precio: 500, Categoria: 4, img:"./Imagenes/Pollo.jpg"},
 ];
 
 let productosHTML = document.getElementById("sectionPlatos");
@@ -82,6 +82,11 @@ const filtrado = (var1) => {
     var1.forEach( item =>{
         let div = document.createElement("div");
         div.classList.add("productos")
+        div.style.backgroundImage = `url(${item.img})`;
+        div.style.backgroundSize = "cover";
+        div.style.backgroundRepeat = "no-repeat";
+        div.style.backgroundPosition = "center";
+        div.style.opacity = "0.9";
         div.innerHTML = `
             <h2>${item.Nombre}</h2>
             <h4>$${item.Precio}</h4>
